506916f6b0113a07aea00e9eb65a82d01ffd1df6
BRYCE SMITH <a.bryce.smith.gmail.com>
2023-12-19 10:35:11 -0500

    nextImg method

--- scripts/bryce.js
+++ scripts/bryce.js
@@ -1,13 +1,23 @@
 "use strict";
 
-
+const bryceImgs = ["Frankie.jpg", "Beethoven.jpg", "emmyBeethoven.jpg", "FrankieLeaf.jpg", "TimmyEmmy.jpg"];
+let imgIndex = 0;
 
 function init() {
-    const nextImgButton = document.querySelector("#next-img-button");
-    let imgNum = 1;
+  const nextImgButton = document.querySelector("#next-img-button");
+  const bryceCurrentImg = document.querySelector("#bryce-current-img");
 
+  function nextImg() {
+    imgIndex++;
 
-    nextImgButton.addEventListener("click", changeImg);
+    while (imgIndex >= bryceImgs.length) {
+      imgIndex -= bryceImgs.length;
+    }
+
+    bryceCurrentImg.src = `images/${bryceImgs[imgIndex]}`;
+  }
+
+  nextImgButton.addEventListener("click", nextImg);
 }
 
 window.onload = init;
