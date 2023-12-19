720cffca6c4217ab1c00a0fa7efdca427b6256ee
Teddy Faulk <FaulkDev@gmail.com>
2023-12-19 11:01:20 -0500

    reformat code

--- js/index.js
+++ js/index.js
@@ -1,10 +1,13 @@
 const generateButton = document.getElementById("generateButton");
 const helloBlock = document.querySelector(".hello-block");
+let allPLength = helloBlock.querySelectorAll("p").length;
 
 generateButton.onclick = function () {
-  if (helloBlock.querySelectorAll("p").length <= 100) {
-    function loop() {
+  function loop() {
+    if (allPLength <= 100) {
       for (let i = 0; i <= 10; i++) {
+        console.log(allPLength);
+
         let hello = document.createElement("p");
         hello.innerText = "Hello!";
 
@@ -17,5 +20,20 @@ generateButton.onclick = function () {
         }
       }
     }
+    if (allPLength >= 100 && allPLength <= 200) {
+      for (let i = 0; i <= 10; i++) {
+        let hello = document.createElement("p");
+        hello.innerText = "Meow!";
+
+        hello.style = `font-size: 1.${i}rem`;
+        helloBlock.appendChild(hello);
+        if (i == 10) {
+          setTimeout(() => {
+            loop();
+          }, 750);
+        }
+      }
+    }
   }
+  loop();
 };
